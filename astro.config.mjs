3c40aacecc88fc913a7e2800dd83e3d502c307d8
beginngingofdays <beginngingofdays@gmail.com>
2024-08-02 01:23:35 +0100

    Company page update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,4 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
 
-export default defineConfig({
-  site: 'https://beginningofdays.github.io',
-  base: 'my-repo',
-})
+// https://astro.build/config
+export default defineConfig({});
