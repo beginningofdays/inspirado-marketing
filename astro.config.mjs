04308a5292db379a7c1102d7290657730e2bb8cb
beginngingofdays <beginngingofdays@gmail.com>
2024-08-02 01:36:07 +0100

    config update repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config'
 
 export default defineConfig({
   site: 'https://beginningofdays.github.io',
-  base: 'beginningofdays',
+  base: 'beginningofdays/inspirado-marketing',
 })
