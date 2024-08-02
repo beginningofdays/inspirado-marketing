5fc76d011897cec609921025ac346204147601cd
beginngingofdays <beginngingofdays@gmail.com>
2024-08-02 12:38:47 +0100

    debugging leaflet.js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
- /* site: 'https://beginningofdays.github.io',
-  base: '/inspirado-marketing',*/
+ site: 'https://beginningofdays.github.io',
+  base: '/inspirado-marketing',
 })
